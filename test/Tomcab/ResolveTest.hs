932f9aa0a4e795e9b8a88d13a18225db3ca29cfd
Brandon Chinn <brandonchinn178@gmail.com>
2022-07-26 19:28:54 -0700

    Add more test TODOs

---
+++ test/Tomcab/ResolveTest.hs
@@ -0,0 +1,19 @@
+{- AUTOCOLLECT.TEST -}
+
+module Tomcab.ResolveTest (
+  -- $AUTOCOLLECT.TEST.export$
+) where
+
+{----- resolveImports -----}
+
+test_todo = "resolve imports: non-existent common stanza"
+test_todo = "resolve imports: merge lists"
+test_todo = "resolve imports: common stanza fields overridden by latter common stanzas"
+test_todo = "resolve imports: common stanza fields overridden by set field"
+
+{----- resolveModules -----}
+
+test_todo = "resolve modules: no wildcards"
+test_todo = "resolve modules: plain wildcards"
+test_todo = "resolve modules: wildcard overridden with explicit"
+test_todo = "resolve modules: wildcard overridden with other-modules"
